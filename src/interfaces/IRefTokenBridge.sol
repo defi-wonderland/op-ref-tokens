d062a2a89e9a528955bd28eb6562f758da70f5dc
Ashitaka <ashitaka@defi.sucks>
2025-05-30 15:35:37 -0300

    feat: skeleton

--- src/interfaces/IRefTokenBridge.sol
+++ src/interfaces/IRefTokenBridge.sol
@@ -7,6 +7,20 @@ pragma solidity 0.8.25;
  */
 interface IRefTokenBridge {
   /**
+   * @notice Data structure for the RefTokenBridge
+   * @param _token The token to be bridged
+   * @param _amount The amount of token to be bridged
+   * @param _recipient The recipient of the bridged token
+   * @param _destinationChainId The destination chain ID
+   */
+  struct RefTokenBridgeData {
+    address _token;
+    uint256 _amount;
+    address _recipient;
+    uint256 _destinationChainId;
+  }
+
+  /**
    * @notice Event emitted when a message is sent
    * @dev If data is empty, just send token to the destination chain
    * @param _token The token to be bridged
@@ -28,36 +42,34 @@ interface IRefTokenBridge {
   );
 
   /**
-   * @notice Data structure for the RefTokenBridge
-   * @param _token The token to be bridged
-   * @param _amount The amount of token to be bridged
-   * @param _recipient The recipient of the bridged token
-   * @param _destinationChainId The destination chain ID
+   * @notice Get the L2 to L2 cross domain messenger address
+   * @return _l2ToL2CDM The L2 to L2 cross domain messenger address
    */
-  struct RefTokenBridgeData {
-    address _token;
-    uint256 _amount;
-    address _recipient;
-    uint256 _destinationChainId;
-  }
+  function L2_To_L2_CDM() external view returns (address _l2ToL2CDM);
 
   /**
-   * @notice Send token to the destination chain and execute in the executor destination chain
+   * @notice Send token to the destination chain and execute in the destination chain executor
    * @param _refTokenBridgeData The data structure for the RefTokenBridge
    * @param _data The data to be executed on the destination chain
    */
-  function sendAndExecute(RefTokenBridgeData calldata _refTokenBridgeData, bytes memory _data) external;
+  function sendAndExecute(RefTokenBridgeData calldata _refTokenBridgeData, bytes memory _data) external payable;
 
   /**
-   * @notice Send token to the destination chain
+   * @notice Send token to the destination chain to transfer tokens to the recipient
    * @param _refTokenBridgeData The data structure for the RefTokenBridge
    */
-  function sendToken(RefTokenBridgeData calldata _refTokenBridgeData) external;
+  function send(RefTokenBridgeData calldata _refTokenBridgeData) external payable;
 
   /**
-   * @notice Relay message from the destination chain
+   * @notice Relay message from the destination chain and execute in the destination chain executor
    * @param _refTokenBridgeData The data structure for the RefTokenBridge
    * @param _data The data to be executed
    */
-  function relayMessage(RefTokenBridgeData calldata _refTokenBridgeData, bytes memory _data) external;
+  function relayAndExecute(RefTokenBridgeData calldata _refTokenBridgeData, bytes memory _data) external;
+
+  /**
+   * @notice Relay message from the destination chain to transfer tokens to the recipient
+   * @param _refTokenBridgeData The data structure for the RefTokenBridge
+   */
+  function relay(RefTokenBridgeData calldata _refTokenBridgeData) external;
 }
