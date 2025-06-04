96b198993b0a9deccad9b57f4869dc35fb2ee532
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2025-06-04 18:25:28 -0300

    feat: add interface and natspec

--- src/contracts/RefToken.sol
+++ src/contracts/RefToken.sol
@@ -6,15 +6,42 @@ import {PredeployAddresses} from '@interop-lib/src/libraries/PredeployAddresses.
 import {Unauthorized} from '@interop-lib/src/libraries/errors/CommonErrors.sol';
 import {IRefTokenBridge} from 'interfaces/IRefTokenBridge.sol';
 
+/**
+ * @title RefToken
+ * @notice A SuperchainERC20-compatible reference token for bridging locked native assets and ERC-20s across OP-Stack
+ *         chains. Issues RefTokens via the RefTokenBridge and enables arbitrary execution on destination chains upon
+ *         receipt.
+ */
 contract RefToken is SuperchainERC20 {
+  /**
+   * @notice The address of the RefTokenBridge contract
+   */
   IRefTokenBridge public immutable REF_TOKEN_BRIDGE;
+
+  /**
+   * @notice The chain id where the native asset is locked
+   */
   uint256 public immutable NATIVE_ASSET_CHAIN_ID;
 
+  /**
+   * @notice The name of the native asset
+   */
   string public NATIVE_ASSET_NAME;
+
+  /**
+   * @notice The symbol of the native asset
+   */
   string public NATIVE_ASSET_SYMBOL;
 
   // TODO: Native asset address and decimals as metadata? No needed in the logic.
 
+  /**
+   * @notice Constructs the RefToken contract
+   * @param _refTokenBridge The address of the RefTokenBridge contract
+   * @param _nativeAssetChainId The chain id where the native asset is locked
+   * @param _nativeAssetName The name of the native asset
+   * @param _nativeAssetSymbol The symbol of the native asset
+   */
   constructor(
     address _refTokenBridge,
     uint256 _nativeAssetChainId,
@@ -27,24 +54,46 @@ contract RefToken is SuperchainERC20 {
     NATIVE_ASSET_SYMBOL = _nativeAssetSymbol;
   }
 
+  /**
+   * @notice Mints RefToken's to the specified address
+   * @param _to The address to mint the RefToken to
+   * @param _amount The amount of RefToken to mint
+   */
   function mint(address _to, uint256 _amount) external {
     if (msg.sender != address(REF_TOKEN_BRIDGE)) revert Unauthorized();
     _mint(_to, _amount);
   }
 
+  /**
+   * @notice Burns RefToken's from the specified address
+   * @param _from The address to burn the RefToken from
+   * @param _amount The amount of RefToken to burn
+   */
   function burn(address _from, uint256 _amount) external {
     if (msg.sender != address(REF_TOKEN_BRIDGE)) revert Unauthorized();
     _burn(_from, _amount);
   }
 
+  /**
+   * @notice The name of the RefToken, composed by a predefined string and the native asset name
+   */
   function name() public view override returns (string memory) {
     return string.concat('RefToken (', NATIVE_ASSET_NAME, ')');
   }
 
+  /**
+   * @notice The symbol of the RefToken, composed by a predefined string and the native asset symbol
+   */
   function symbol() public view override returns (string memory) {
     return string.concat('REF-', NATIVE_ASSET_SYMBOL);
   }
 
+  /**
+   * @notice Mints RefToken's to the specified address or unlocks the native asset if the caller is the
+   *  SuperchainTokenBridge and the chain id is the native asset one
+   * @param _to The address to mint the RefToken to
+   * @param _amount The amount of RefToken to mint
+   */
   function _mint(address _to, uint256 _amount) internal override {
     if (msg.sender == PredeployAddresses.SUPERCHAIN_TOKEN_BRIDGE && block.chainid == NATIVE_ASSET_CHAIN_ID) {
       REF_TOKEN_BRIDGE.unlock(_to, _amount);
---
+++ src/interfaces/IRefToken.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+interface IRefToken {
+  /**
+   * @notice The address of the RefTokenBridge contract
+   */
+  function REF_TOKEN_BRIDGE() external view returns (address);
+
+  /**
+   * @notice The chain id where the native asset is locked
+   */
+  function NATIVE_ASSET_CHAIN_ID() external view returns (uint256);
+
+  /**
+   * @notice The name of the native asset
+   */
+  function NATIVE_ASSET_NAME() external view returns (string memory);
+
+  /**
+   * @notice The symbol of the native asset
+   */
+  function NATIVE_ASSET_SYMBOL() external view returns (string memory);
+
+  /**
+   * @notice Mints RefToken's to the specified address
+   * @param _to The address to mint the RefToken to
+   * @param _amount The amount of RefToken to mint
+   */
+  function mint(address _to, uint256 _amount) external;
+
+  /**
+   * @notice Burns RefToken's from the specified address
+   * @param _from The address to burn the RefToken from
+   * @param _amount The amount of RefToken to burn
+   */
+  function burn(address _from, uint256 _amount) external;
+
+  /**
+   * @notice The name of the RefToken, composed by a predefined string and the native asset name
+   */
+  function name() external view returns (string memory);
+
+  /**
+   * @notice The symbol of the RefToken, composed by a predefined string and the native asset symbol
+   */
+  function symbol() external view returns (string memory);
+}
