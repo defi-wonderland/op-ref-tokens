830082b93fbcd3357d32b707611f6c8080e7eefe
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2025-06-18 12:22:44 -0300

    chore: param name

--- src/interfaces/external/IExecutor.sol
+++ src/interfaces/external/IExecutor.sol
@@ -8,14 +8,14 @@ pragma solidity 0.8.25;
 interface IExecutor {
   /**
    * @notice Executes the given data
-   * @param _refToken The RefToken address
-   * @param _recipient The recipient of the RefToken after execution
-   * @param _amount The approved amount of RefToken
+   * @param _token The token to be executed, either the native asset or the RefToken
+   * @param _recipient The recipient of the token after execution
+   * @param _amount The amount of token to be executed
    * @param _destinationChainId The chain Id of the next step after execution
    * @param _data The data to execute
    */
   function execute(
-    address _refToken,
+    address _token,
     address _recipient,
     uint256 _amount,
     uint256 _destinationChainId,
